3aa91eb08572f1962a072d5beff1ad3d0238b47c
Michael Kessler <michael.kessler@screenconcept.ch>
2009-03-09 14:24:04 +0100

    railsdav initialization of http methods for different rails version merged

--- vendor/railsdav/init.rb
+++ vendor/railsdav/init.rb
@@ -3,8 +3,15 @@ $:.unshift File.join(File.dirname(__FILE__), 'lib/')
 
 require 'railsdav'
 
-ActionController::AbstractRequest::HTTP_METHODS << 'propfind'
-ActionController::AbstractRequest::HTTP_METHODS << 'lock'
+if defined? ActionController::AbstractRequest::HTTP_METHODS
+  ActionController::AbstractRequest::HTTP_METHODS << 'propfind'
+  ActionController::AbstractRequest::HTTP_METHODS << 'lock'
+end
+
+if defined? ActionController::ACCEPTED_HTTP_METHODS
+  ActionController::ACCEPTED_HTTP_METHODS.add('propfind')
+  ActionController::ACCEPTED_HTTP_METHODS.add('lock')
+end
 
 ActionController::Base.send(:include, Railsdav)
 ActionController::Base.send(:include, Railsdav::ActAsFileWebDav)
