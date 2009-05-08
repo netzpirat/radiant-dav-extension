9090e60352906b70390b883de4943dd667ff813f
Michael Kessler <michi@netzpiraten.ch>
2009-05-08 20:08:32 +0200

    Regenerate asset on write existing resource

--- app/models/paperclipped/radiant_asset_resource.rb
+++ app/models/paperclipped/radiant_asset_resource.rb
@@ -22,17 +22,18 @@ class Paperclipped::RadiantAssetResource < RadiantFileResource
 
   def data
     File.new(absolute_asset_path)
-
   end
 
   def write!(content)
-    File.open(absolute_asset_path, 'w') {|asset| asset.write(contebt) }
+    File.open(absolute_asset_path, 'w') {|asset| asset.write(content) }
+    record.asset.reprocess!
+    record.save
   end
-
+ 
   private
 
     def absolute_asset_path
       "#{RAILS_ROOT}/public/assets/#{@record.id}/#{@record.asset_file_name}"
     end
-  
+
 end
