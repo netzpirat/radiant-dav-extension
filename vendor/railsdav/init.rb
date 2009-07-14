470c2a8038d5c4aff8ade23d7cb8b6461d0d77a8
Michael Kessler <michael.kessler@screenconcept.ch>
2009-07-14 15:03:11 +0200

    Make compatible with Radiant 0.8.0

--- vendor/railsdav/init.rb
+++ vendor/railsdav/init.rb
@@ -3,6 +3,12 @@ $:.unshift File.join(File.dirname(__FILE__), 'lib/')
 
 require 'railsdav'
 
+if defined? ActionController::Request::HTTP_METHODS
+  ActionController::Request::HTTP_METHODS << 'propfind'
+  ActionController::Request::HTTP_METHODS << 'lock'
+  ActionController::Request::HTTP_METHOD_LOOKUP = ActionController::Request::HTTP_METHODS.inject({}) { |h, m| h[m] = h[m.upcase] = m.to_sym; h }
+end
+
 if defined? ActionController::AbstractRequest::HTTP_METHODS
   ActionController::AbstractRequest::HTTP_METHODS << 'propfind'
   ActionController::AbstractRequest::HTTP_METHODS << 'lock'
